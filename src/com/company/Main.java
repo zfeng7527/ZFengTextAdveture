c8348a4a49cb59a4efb2137dae6b4839ca609b11
zfeng7527 <zfeng7527@bths.edu>
2018-12-13 00:06:42 -0500

    |:<

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,10 +18,10 @@ public static boolean gameOn = true;
         Scanner move = new Scanner(System.in);
         System.out.println("Use wasd to move(LowerCase)*Going Out Of Bounds WILL BREAK* \n< > = player room | [ ] = empty room | [☼] = winning room *notimplemented.\n\n\u001B[31mYou wake up in an unfamiliar place. NOW GO DO SOMETHING USEFUL WITH YOR LIFE");
         while (gameOn){
-            System.out.println("\u001B[30m");
+            System.out.println("\u001B[30m"); //Changes color back to black.
             String move1 = move.nextLine();
             switch(move1){
-                case "w"s:{ //North
+                case "w":{ //North
                     map1.Map[map1.py][map1.px] = "[" + map1.Map[map1.py][map1.px].substring(1,2) + "]";
                     map1.py--;
                     map1.SetPlayer();
